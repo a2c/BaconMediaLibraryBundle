1a54d26bbf2a49c73136980d4b01e8ca49290070
Gustavo Henrique Conci <gustavo.conci@a2c.com.br>
2016-09-08 15:23:17 -0300

    evento fechar no popup

--- Resources/public/js/ckeditor.js
+++ Resources/public/js/ckeditor.js
@@ -1,15 +1,21 @@
 jQuery(document).ready(function($) {
 
+  $('[data-dismiss="modal"]').on('click', function() {
+    window.close();
+  });
+
 	bacon_upload_bundle(
 		function() {
 			$('#return_value_save').on('click', function() {
 				var srcImage = $('input.value_image_select:checked').data('src');
 				$(window.opener.document).find('#cke_118_textInput').val(srcImage);
+
 				window.close();
 			});
 		},
 		function(image) {
 			$(window.opener.document).find('#cke_118_textInput').val(image.srcOriginal);
+
 			window.close();
 		}
 	);
